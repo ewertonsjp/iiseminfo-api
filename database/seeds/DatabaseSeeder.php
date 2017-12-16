25cc8980069e895442de4b27953ff801dd6f6b4f
Ewerton Costa <ewertonsjp@gmail.com>
2017-12-16 10:07:22 -0300

    cadastra mais eventos no seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -18,5 +18,29 @@ class DatabaseSeeder extends Seeder
            'descricao' => 'Inovação, empreendedorismo e aceleração de ideias no semiárido.',
            'img' => 'http://9solucoes.com.br/seminfo/images/palestras/aislan1.jpg',
            'likes' => 0]);         
+        Event::create(
+            ['tipo' => 'Palestra',
+            'autor' => 'Prof. Rômulo Camara - Univasf',
+            'descricao' => 'Desenvolvimento de projetos para a região utilizando hardware livre.',
+            'img' => 'http://9solucoes.com.br/seminfo/images/palestras/open.jpg',
+            'likes' => 0]);
+        Event::create(
+            ['tipo' => 'Palestra',
+            'autor' => 'Prof. Fábio Rolim - IFPI',
+            'descricao' => 'Projeto HelpFish - 2º colocado no Hackaton da Embrapa.',
+            'img' => 'http://9solucoes.com.br/seminfo/images/palestras/rolim.jpg',
+            'likes' => 0]);
+        Event::create(
+            ['tipo' => 'Mesa Redonda',
+            'autor' => 'Associação de ...',
+            'descricao' => 'Discussão sobre os desafios do semiárido na região de Paulistana.',
+            'img' => 'http://1.bp.blogspot.com/-X79_uFTend8/T7ZGmMaADII/AAAAAAAAEek/pHUlwld15gU/s1600/mesa-redonda.jpg',
+            'likes' => 0]);            
+        Event::create(
+            ['tipo' => 'Hackathon',
+            'autor' => 'Maykol Sampaio',
+            'descricao' => 'Corrida para montar ideia inovadora para os desafios discutidos no evento.',
+            'img' => 'http://19g6dy4by8jx1b5cx74fh0f2-wpengine.netdna-ssl.com/wp-content/uploads/2015/12/Hackathon-cartoon-Irina-Bezyanova-Shutterstock.jpg',
+            'likes' => 0]);                                   
     }
 }
